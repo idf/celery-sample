72089f950e68b3f0efb22eb71f2c2b96a717ca1e
Daniel D. Zhang <dzhang.idf@gmail.com>
2017-04-20 10:28:07 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,9 @@
-function celery_start() {
-    celery -A consumer worker --loglevel=info;
+celery_start() {
+    # start the worker
+    celery -A consumer worker --loglevel=info
     # -A for celery application name
 }
 
-function producer_run() {
-    python producer.py;
+producer_run() {
+    python producer.py
 }
